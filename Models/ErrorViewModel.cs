9658abd2239931176df0d7cd5a988b21d862c673
Arturo-Vaneza <Arturo-Vaneza@JAVKRISVECTOR45>
2025-11-18 18:56:10 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppToDo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
